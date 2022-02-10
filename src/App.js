fd7c1c8702c7198aaa8bb1b1bcea2a07fe076502
gwak <jaewoo010207@gmail.com>
2022-02-10 19:11:30 +0900

    view box size

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import Box from "./components/Box";
+
 function App() {
-  return <div className="App">hi</div>;
+  const [stage, setStage] = useState(3);
+  const [stage_list, setStage_list] = useState([]);
+
+  const init = (stage) => {
+    let arr = [];
+    for (let i = 0; i < stage * stage; i++) {
+      arr.push(i + 1);
+    }
+    setStage_list(arr);
+  };
+
+  useEffect(() => {
+    console.log(stage);
+    init(stage);
+  }, []);
+  return (
+    <div>
+      <View stage={stage}>
+        {stage_list.map((st) => (
+          <Box key={st}></Box>
+        ))}
+      </View>
+    </div>
+  );
 }
+
+const View = styled.div`
+  display: grid;
+  width: 400px;
+  height: 400px;
+  grid-template-columns: repeat(${(props) => props.stage}, 1fr);
+  gap: 1px;
+`;
 
 export default App;
---
+++ src/components/Box.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import styled from "styled-components";
+const Box = () => {
+  return <Space>box</Space>;
+};
+
+const Space = styled.div`
+  border: 1px solid black;
+  text-align: center;
+`;
+
+export default Box;
