2ee106ef32747cf44108cdc99c915b2c3872b84c
gwak <jaewoo010207@gmail.com>
2022-02-15 00:29:12 +0900

    색깔 랜덤 및 기본 배치

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import styled from "styled-components";
 import Box from "./components/Box";
 
 let STAGE_NUM = 2; // 2 ~ 8
-
+let STAGE_MAX = 8;
 function App() {
   const [stage, setStage] = useState(2);
   const [stage_list, setStage_list] = useState([]);
   const [other, setOther] = useState();
+  const [color, setColor] = useState();
+  const [randColor, setRandColor] = useState();
   const init = (stage) => {
     let arr = [];
     for (let i = 0; i < stage * stage; i++) {
@@ -15,16 +17,33 @@ function App() {
     }
     setStage_list(arr);
     randomGen();
+    getRandomColor();
   };
 
   const randomGen = () => {
-    const num = Math.floor(Math.random() * stage + 1);
+    const num = Math.floor(Math.random() * stage * stage + 1);
     setOther(num);
+  };
+
+  const getRandomColor = () => {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    var code = "";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+      if (i < 5) {
+        code += letters[Math.floor(Math.random() * 16)];
+      }
+    }
+    const baseColor = `#${code}9`;
+    console.log(baseColor);
+    setColor(baseColor);
+    setRandColor(color);
   };
   const onClicked = (e) => {
     if (e.target.id == other) {
       console.log("clicked!", e.target.id);
-      if (STAGE_NUM < 8) {
+      if (STAGE_NUM < STAGE_MAX) {
         STAGE_NUM += 1;
       }
       setStage(STAGE_NUM);
@@ -37,12 +56,19 @@ function App() {
     console.log("now stage", stage);
     init(stage);
     console.log("other", other);
-  }, [stage, other]);
+  }, [stage]);
   return (
     <div>
       <View stage={stage}>
         {stage_list.map((st) => (
-          <Box key={st} id={st} onClick={onClicked}></Box>
+          <Box
+            key={st}
+            id={st}
+            randomNum={other}
+            randomColor={randColor}
+            color={color}
+            onClick={onClicked}
+          ></Box>
         ))}
       </View>
     </div>
--- src/components/Box.js
+++ src/components/Box.js
@@ -1,15 +1,23 @@
 import { useState } from "react";
 import styled from "styled-components";
-const Box = ({ id, onClick }) => {
+const Box = ({ id, onClick, randomNum, randomColor, color }) => {
   return (
-    <Space id={id} onClick={onClick}>
-      box
+    <Space
+      id={id}
+      onClick={onClick}
+      isRandom={id == randomNum}
+      randomColor={randomColor}
+      color={color}
+    >
+      {id == randomNum ? "rd" : "box"}
     </Space>
   );
 };
 
 const Space = styled.div`
   border: 1px solid black;
+  background-color: ${(props) =>
+    props.isRandom ? props.color : props.randomColor};
   text-align: center;
 `;
 
