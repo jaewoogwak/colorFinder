e6bcf4264bc0ffb6fc2315dc006c6773bc57c135
gwak <jaewoo010207@gmail.com>
2022-02-15 11:41:58 +0900

    랜덤색상 프로토타입 구현 및 툴바 구현

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import styled from "styled-components";
 import Box from "./components/Box";
 
 let STAGE_NUM = 2; // 2 ~ 8
-let STAGE_MAX = 8;
+let STAGE_MAX = 9;
 function App() {
   const [stage, setStage] = useState(2);
+  const [size, setSize] = useState(2);
   const [stage_list, setStage_list] = useState([]);
   const [other, setOther] = useState();
   const [color, setColor] = useState();
   const [randColor, setRandColor] = useState();
+  const [score, setScore] = useState(0);
+
   const init = (stage) => {
     let arr = [];
-    for (let i = 0; i < stage * stage; i++) {
+    for (let i = 0; i < size * size; i++) {
       arr.push(i + 1);
     }
     setStage_list(arr);
@@ -21,30 +24,35 @@ function App() {
   };
 
   const randomGen = () => {
-    const num = Math.floor(Math.random() * stage * stage + 1);
+    const num = Math.floor(Math.random() * size * size + 1);
     setOther(num);
   };
 
   const getRandomColor = () => {
     var letters = "0123456789ABCDEF";
     var color = "#";
-    var code = "";
+    let rColor = "";
     for (var i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
-      if (i < 5) {
-        code += letters[Math.floor(Math.random() * 16)];
+    }
+    for (let i = 0; i < color.length; i++) {
+      if (isNaN(color[i]) == false) {
+        rColor += (color[i] + 9) % 10;
+      } else {
+        rColor += color[i];
       }
     }
-    const baseColor = `#${code}9`;
-    console.log(baseColor);
-    setColor(baseColor);
-    setRandColor(color);
+    setColor(color);
+    setRandColor(rColor);
   };
+
   const onClicked = (e) => {
     if (e.target.id == other) {
       console.log("clicked!", e.target.id);
-      if (STAGE_NUM < STAGE_MAX) {
-        STAGE_NUM += 1;
+      STAGE_NUM += 1;
+      setScore((prev) => prev + stage);
+      if (STAGE_NUM <= STAGE_MAX) {
+        setSize(STAGE_NUM);
       }
       setStage(STAGE_NUM);
     } else {
@@ -59,7 +67,11 @@ function App() {
   }, [stage]);
   return (
     <div>
-      <View stage={stage}>
+      <InfoBar>
+        stage : {stage}
+        score : {score}
+      </InfoBar>
+      <View stage={stage} size={size}>
         {stage_list.map((st) => (
           <Box
             key={st}
@@ -79,7 +91,7 @@ const View = styled.div`
   display: grid;
   width: 400px;
   height: 400px;
-  grid-template-columns: repeat(${(props) => props.stage}, 1fr);
+  grid-template-columns: repeat(${(props) => props.size}, 1fr);
   gap: 1px;
 `;
 
