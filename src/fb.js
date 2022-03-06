25a907e74a401108103adfa508bd965993d6509b
gwak <jaewoo010207@gmail.com>
2022-03-06 16:30:49 +0900

    key #4

--- src/fb.js
+++ src/fb.js
@@ -1,18 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import firebaseConfig from "./key";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCoHgLlDyMRSXX7FcrCHVQ2B1JzCiVS6Po",
-  authDomain: "rankingdb-a29fe.firebaseapp.com",
-  projectId: "rankingdb-a29fe",
-  storageBucket: "rankingdb-a29fe.appspot.com",
-  messagingSenderId: "730276427861",
-  appId: "1:730276427861:web:fb69d92b6482926617fcdc",
-};
 
 export default firebaseConfig;
 
